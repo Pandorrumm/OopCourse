f87eb8967ff57ed88dc97d8907c1d36426833336
VitalyPavlov <fankit8686@gmail.com>
2026-02-03 09:30:00 +0700

    Create non-static methods for Vector

--- Vector/src/ru/nsk/pavlov/vector/Vector.java
+++ Vector/src/ru/nsk/pavlov/vector/Vector.java
@@ -29,6 +29,10 @@ public class Vector {
         components = Arrays.copyOf(array, Math.min(n, array.length));
     }
 
+    public double[] getComponents() {
+        return components;
+    }
+
     public int getSize() {
         return components.length;
     }
@@ -39,8 +43,90 @@ public class Vector {
 
         stringBuilder.append("{");
 
+        for (int i = 0; i < components.length; i++) {
+            if (i != components.length - 1) {
+                stringBuilder.append(components[i]);
+                stringBuilder.append(", ");
+            } else {
+                stringBuilder.append(components[i]);
+            }
+        }
+
         stringBuilder.append("}");
 
         return stringBuilder.toString();
+    }
+
+    public Vector addingVector(Vector vector) {
+        if (components.length != vector.components.length) {
+            throw new IllegalArgumentException("Vectors of different dimensions");
+        }
+
+        double[] result = new double[components.length];
+
+        for (int i = 0; i < vector.components.length; i++) {
+            result[i] = components[i] + vector.components[i];
+        }
+
+        return new Vector(result);
+    }
+
+    public Vector subtractionVector(Vector vector) {
+        if (components.length != vector.components.length) {
+            throw new IllegalArgumentException("Vectors of different dimensions");
+        }
+
+        double[] result = new double[components.length];
+
+        for (int i = 0; i < vector.components.length; i++) {
+            result[i] = components[i] - vector.components[i];
+        }
+
+        return new Vector(result);
+    }
+
+    public Vector multiplicationByScalar(double scalar) {
+        double[] result = new double[components.length];
+
+        for (int i = 0; i < components.length; i++) {
+            result[i] = components[i] * scalar;
+        }
+
+        return new Vector(result);
+    }
+
+    public Vector reverseVector() {
+        double[] result = new double[components.length];
+
+        for (int i = 0; i < components.length; i++) {
+            result[i] = components[i] * -1;
+        }
+
+        return new Vector(result);
+    }
+
+    public double getVectorLength() {
+        double result = 0;
+
+        for (double component : components) {
+            result += component * component;
+        }
+
+        return Math.sqrt(result);
+    }
+
+    public double getComponentByIndex(int index) {
+        if (index < 0) {
+            throw new IllegalArgumentException("Dimension of the array must be >= 0");
+        }
+
+        if (index >= components.length) {
+            throw new IllegalArgumentException("The index goes beyond the array");
+        }
+        return components[index];
+    }
+
+    public void setComponentByIndex(int index, double component) {
+        components[index] = component;
     }
 }
--- Vector/src/ru/nsk/pavlov/vector_main/Main.java
+++ Vector/src/ru/nsk/pavlov/vector_main/Main.java
@@ -1,4 +1,24 @@
-package ru.nsk.pavlov;
+package ru.nsk.pavlov.vector_main;
+
+import ru.nsk.pavlov.vector.Vector;
 
 public class Main {
+    public static void main(String[] args) {
+        Vector vector1 = new Vector(new double[]{1, 3, 5});
+        Vector vector2 = new Vector(new double[]{2, 0, 1});
+
+        System.out.println(vector1);
+        System.out.println(vector1.addingVector(vector2));
+        System.out.println(vector1.subtractionVector(vector2));
+        System.out.println(vector1.multiplicationByScalar(2));
+
+        System.out.println(vector1.reverseVector());
+
+        System.out.println(vector1.getVectorLength());
+
+        System.out.println(vector1.getComponentByIndex(2));
+
+        vector1.setComponentByIndex(0, 5);
+        System.out.println(vector1);
+    }
 }
