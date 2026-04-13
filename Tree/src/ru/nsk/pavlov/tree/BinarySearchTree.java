bf999fee00dfe7ee62d9b4430f7868ebd009f052
VitalyPavlov <fankit8686@gmail.com>
2026-04-13 09:24:27 +0700

    Refactoring BinarySearchTree script. Create toString methods

--- Tree/src/ru/nsk/pavlov/tree/BinarySearchTree.java
+++ Tree/src/ru/nsk/pavlov/tree/BinarySearchTree.java
@@ -1,224 +1,283 @@
 package ru.nsk.pavlov.tree;
 
-import ru.nsk.pavlov.tree_node.TreeNode;
+import java.util.*;
+import java.util.function.Consumer;
 
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.Stack;
-
-public class BinarySearchTree<E extends Comparable<E>> {
+public class BinarySearchTree<E> {
     private TreeNode<E> root;
-    private int nodeCount;
+    private int size;
+    private final Comparator<E> comparator;
 
-    public void insert(E x) {
+    private static class TreeNode<E> {
+        private TreeNode<E> left;
+        private TreeNode<E> right;
+        private final E data;
+
+        private TreeNode(E data) {
+            this.data = data;
+        }
+    }
+
+    public BinarySearchTree() {
+        this.root = null;
+        this.comparator = null;
+        this.size = 0;
+    }
+
+    public BinarySearchTree(Comparator<E> comparator) {
+        this.root = null;
+        this.comparator = comparator;
+        this.size = 0;
+    }
+
+    private int compare(E leftElement, E rightElement) {
+        if (leftElement == null && rightElement == null) {
+            return 0;
+        }
+
+        if (leftElement == null) {
+            return -1;
+        }
+
+        if (rightElement == null) {
+            return 1;
+        }
+
+        if (comparator != null) {
+            return comparator.compare(leftElement, rightElement);
+        } else {
+            //noinspection unchecked
+            return ((Comparable<E>) leftElement).compareTo(rightElement);
+        }
+    }
+
+    public void add(E element) {
+        if (element == null) {
+            throw new NullPointerException("Cannot add null element");
+        }
+
         if (root == null) {
-            root = new TreeNode<>(x);
-            nodeCount++;
+            root = new TreeNode<>(element);
+
+            size++;
             return;
         }
 
         TreeNode<E> current = root;
+        TreeNode<E> parent = null;
 
-        while (true) {
-            if (x.compareTo(current.getData()) < 0) {
-                if (current.getLeft() == null) {
-                    current.setLeft(new TreeNode<>(x));
+        int comparisonResult = 0;
 
-                    nodeCount++;
+        while (current != null) {
+            parent = current;
+            comparisonResult = compare(element, current.data);
 
-                    return;
-                } else {
-                    current = current.getLeft();
-                }
-            } else {
-                if (current.getRight() == null) {
-                    current.setRight(new TreeNode<>(x));
-
-                    nodeCount++;
-
-                    return;
-                } else {
-                    current = current.getRight();
-                }
+            if (comparisonResult < 0) {
+                current = current.left;
+            } else if (comparisonResult > 0) {
+                current = current.right;
             }
         }
+
+        if (comparisonResult < 0) {
+            parent.left = new TreeNode<>(element);
+        } else {
+            parent.right = new TreeNode<>(element);
+        }
+
+        size++;
     }
 
-    public TreeNode<E> findNode(E x) {
+    public boolean contains(Object element) {
+        if (element == null) {
+            return false;
+        }
+
+        @SuppressWarnings("unchecked")
+        E e = (E) element;
+
         TreeNode<E> current = root;
 
         while (current != null) {
-            int comparisonResult = x.compareTo(current.getData());
+            int comparisonResult = compare(e, current.data);
 
             if (comparisonResult < 0) {
-                current = current.getLeft();
+                current = current.left;
             } else if (comparisonResult > 0) {
-                current = current.getRight();
+                current = current.right;
             } else {
-                return current;
+                return true;
             }
         }
 
-        return null;
+        return false;
     }
 
-    public boolean delete(E x) {
-        TreeNode<E> nodeToDelete = findNode(x);
+    public boolean remove(E element) {
+        if (element == null || root == null) {
+            return false;
+        }
+
+        TreeNode<E> nodeToDelete = root;
+        TreeNode<E> parent = null;
+
+        int comparisonResult;
+
+        while (nodeToDelete != null) {
+            comparisonResult = compare(element, nodeToDelete.data);
+
+            if (comparisonResult < 0) {
+                parent = nodeToDelete;
+                nodeToDelete = nodeToDelete.left;
+            } else if (comparisonResult > 0) {
+                parent = nodeToDelete;
+                nodeToDelete = nodeToDelete.right;
+            } else {
+                break;
+            }
+        }
 
         if (nodeToDelete == null) {
             return false;
         }
 
-        TreeNode<E> parent = findParent(nodeToDelete);
-
-        if (nodeToDelete.getLeft() == null && nodeToDelete.getRight() == null) {
-            replaceChild(parent, nodeToDelete, null);
-        } else if (nodeToDelete.getLeft() == null) {
-            replaceChild(parent, nodeToDelete, nodeToDelete.getRight());
-        } else if (nodeToDelete.getRight() == null) {
-            replaceChild(parent, nodeToDelete, nodeToDelete.getLeft());
+        if (nodeToDelete.left == null) {
+            replaceChild(parent, nodeToDelete, nodeToDelete.right);
+        } else if (nodeToDelete.right == null) {
+            replaceChild(parent, nodeToDelete, nodeToDelete.left);
         } else {
-            TreeNode<E> minNode = findMinimumNode(nodeToDelete.getRight());
-            TreeNode<E> minNodeParent = findParent(minNode);
+            TreeNode<E> replacement = nodeToDelete.right;
+            TreeNode<E> replacementParent = nodeToDelete;
 
-            if (minNode != nodeToDelete.getRight()) {
-                if (minNodeParent.getLeft() == minNode) {
-                    minNodeParent.setLeft(minNode.getRight());
-                } else {
-                    minNodeParent.setRight(minNode.getRight());
-                }
-
-                minNode.setRight(nodeToDelete.getRight());
+            while (replacement.left != null) {
+                replacementParent = replacement;
+                replacement = replacement.left;
             }
 
-            minNode.setLeft(nodeToDelete.getLeft());
+            if (replacement != nodeToDelete.right) {
+                replacementParent.left = replacement.right;
+                replacement.right = nodeToDelete.right;
+            }
 
-            replaceChild(parent, nodeToDelete, minNode);
+            replacement.left = nodeToDelete.left;
+
+            replaceChild(parent, nodeToDelete, replacement);
         }
 
-        nodeCount--;
+        size--;
 
         return true;
     }
 
-    public int getNodeCount() {
-        return nodeCount;
+    public int getSize() {
+        return size;
     }
 
-    public void preOrderRecursiveSearch() {
-        preOrderRecursiveSearch(root);
+    public void forEachPreOrder(Consumer<E> action) {
+        forEachPreOrderRecursive(root, action);
     }
 
-    private void preOrderRecursiveSearch(TreeNode<E> node) {
+    private void forEachPreOrderRecursive(TreeNode<E> node, Consumer<E> action) {
         if (node == null) {
             return;
         }
 
-        System.out.print(node);
+        action.accept(node.data);
 
-        preOrderRecursiveSearch(node.getLeft());
-        preOrderRecursiveSearch(node.getRight());
+        forEachPreOrderRecursive(node.left, action);
+        forEachPreOrderRecursive(node.right, action);
     }
 
-    public void preOrderSearch() {
+    public void forEachPreOrderIterative(Consumer<E> action) {
         if (root == null) {
             return;
         }
 
-        Stack<TreeNode<E>> stack = new Stack<>();
-
+        Deque<TreeNode<E>> stack = new ArrayDeque<>();
         stack.push(root);
 
         while (!stack.isEmpty()) {
             TreeNode<E> current = stack.pop();
+            action.accept(current.data);
 
-            System.out.print(current);
-
-            if (current.getRight() != null) {
-                stack.push(current.getRight());
+            if (current.right != null) {
+                stack.push(current.right);
             }
 
-            if (current.getLeft() != null) {
-                stack.push(current.getLeft());
+            if (current.left != null) {
+                stack.push(current.left);
             }
         }
     }
 
-    public void breadthFirstSearch() {
+    public void forEachBFS(Consumer<E> action) {
         if (root == null) {
             return;
         }
 
         Queue<TreeNode<E>> queue = new LinkedList<>();
-
         queue.offer(root);
 
         while (!queue.isEmpty()) {
             TreeNode<E> current = queue.poll();
+            action.accept(current.data);
 
-            System.out.print(current);
-
-            if (current.getLeft() != null) {
-                queue.offer(current.getLeft());
+            if (current.left != null) {
+                queue.offer(current.left);
             }
 
-            if (current.getRight() != null) {
-                queue.offer(current.getRight());
+            if (current.right != null) {
+                queue.offer(current.right);
             }
         }
-    }
-
-    private TreeNode<E> findParent(TreeNode<E> target) {
-        if (target == null || target == root) {
-            return null;
-        }
-
-        TreeNode<E> current = root;
-
-        while (current != null) {
-            if (current.getLeft() == target || current.getRight() == target) {
-                return current;
-            }
-
-            int comparisonResult = target.getData().compareTo(current.getData());
-
-            if (comparisonResult < 0) {
-                current = current.getLeft();
-            } else {
-                current = current.getRight();
-            }
-        }
-
-        return null;
-    }
-
-    private TreeNode<E> findMinimumNode(TreeNode<E> node) {
-        while (node.getLeft() != null) {
-            node = node.getLeft();
-        }
-
-        return node;
     }
 
     private void replaceChild(TreeNode<E> parent, TreeNode<E> oldChild, TreeNode<E> newChild) {
         if (parent == null) {
             root = newChild;
-        } else if (parent.getLeft() == oldChild) {
-            parent.setLeft(newChild);
+        } else if (parent.left == oldChild) {
+            parent.left = newChild;
         } else {
-            parent.setRight(newChild);
+            parent.right = newChild;
         }
     }
 
-    public void printTree() {
-        printTree(root);
-    }
-
-    public void printTree(TreeNode<E> node) {
-        if (node != null) {
-            printTree(node.getLeft());
-            System.out.print(node);
-            printTree(node.getRight());
+    @Override
+    public String toString() {
+        if (root == null) {
+            return "[]";
         }
+
+        StringBuilder sb = new StringBuilder();
+        sb.append('[');
+
+        Queue<TreeNode<E>> queue = new LinkedList<>();
+        queue.offer(root);
+
+        boolean isFirst = true;
+
+        while (!queue.isEmpty()) {
+            TreeNode<E> current = queue.poll();
+
+            if (!isFirst) {
+                sb.append(", ");
+            }
+
+            sb.append(current.data);
+
+            isFirst = false;
+
+            if (current.left != null) {
+                queue.offer(current.left);
+            }
+
+            if (current.right != null) {
+                queue.offer(current.right);
+            }
+        }
+
+        sb.append(']');
+
+        return sb.toString();
     }
 }
--- Tree/src/ru/nsk/pavlov/tree_main/Main.java
+++ Tree/src/ru/nsk/pavlov/tree_main/Main.java
@@ -1,57 +1,42 @@
 package ru.nsk.pavlov.tree_main;
 
 import ru.nsk.pavlov.tree.BinarySearchTree;
-import ru.nsk.pavlov.tree_node.TreeNode;
 
 public class Main {
     public static void main(String[] args) {
         BinarySearchTree<Integer> tree = new BinarySearchTree<>();
 
-        tree.insert(10);
-        tree.insert(50);
-        tree.insert(20);
-        tree.insert(40);
-        tree.insert(15);
-        tree.insert(5);
-        tree.insert(100);
+        tree.add(10);
+        tree.add(50);
+        tree.add(20);
+        tree.add(40);
+        tree.add(15);
+        tree.add(5);
+        tree.add(100);
 
-        tree.printTree();
-
-        System.out.println();
-        System.out.println("Node count: " + tree.getNodeCount());
+        System.out.println(tree);
 
         System.out.println();
-        System.out.println("Find node with the value 20");
-
-        TreeNode<Integer> findNode = tree.findNode(20);
-
-        if (findNode != null) {
-            System.out.println("The node with the value 20 has been found!");
-            System.out.println("Note data: " + findNode.getData());
-        } else {
-            System.out.println("Node not found");
-        }
+        System.out.println("Node count: " + tree.getSize());
 
         System.out.println();
-        System.out.println("Delete node with the value 5: " + tree.delete(5));
-        System.out.println("Node count: " + tree.getNodeCount());
-        tree.printTree();
+        System.out.println("Found node with the value 20: " + tree.contains(20));
+
         System.out.println();
+        System.out.println("Delete node with the value 5: " + tree.remove(5));
+        System.out.println("Node count: " + tree.getSize());
+        System.out.println(tree);
 
         System.out.println();
         System.out.println("Preorder tree depth first search with recursion:");
-        tree.preOrderRecursiveSearch();
-        System.out.println();
+        tree.forEachPreOrder(value -> System.out.println(value + " "));
 
         System.out.println();
         System.out.println("Preorder tree depth first search:");
-        tree.preOrderSearch();
-        System.out.println();
-
+        tree.forEachPreOrderIterative(value -> System.out.println(value + " "));
 
         System.out.println();
         System.out.println("Breadth first search:");
-        tree.breadthFirstSearch();
-        System.out.println();
+        tree.forEachBFS(value -> System.out.println(value + " "));
     }
 }
